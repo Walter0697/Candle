d12c1ce7196aaa4cd13ac60c1e07c0887116b3d7
Walter Cheng <walterpakwai@live.com>
2025-09-10 19:57:16 -0400

    fix: attempt

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Increase the maximum file size for precaching
-  maximumFileSizeToCacheInBytes: 8 * 1024 * 1024, // 8MB (7.43MB bundle)
+  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB (7.43MB bundle)
   
   // Configure which files to precache
   globPatterns: [
