6c3855b8b8c12f0af66c0e0ce8781bb3101acfd9
Walter Cheng <walterpakwai@live.com>
2025-09-10 21:15:19 -0400

    fix: realized npm run deploy is the error one

---
+++ workbook-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // Increase the maximum file size for precaching
+  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB (7.43MB bundle)
+  
+  // Configure which files to precache
+  globPatterns: [
+    '**/*.{js,css,html,ico,png,svg,woff,woff2,ttf,eot}'
+  ],
+  
+  // Exclude source maps and other large files that shouldn't be precached
+  globIgnores: [
+    '**/*.map',
+    '**/node_modules/**/*'
+  ],
+  
+  // Configure runtime caching
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60 // 1 year
+        }
+      }
+    }
+  ]
+};
